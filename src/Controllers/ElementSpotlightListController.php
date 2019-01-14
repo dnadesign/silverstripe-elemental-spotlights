c89bb4be32b0fb539179e4c47dd635c805428cc5
Georgy Malanichev <georgy@malanichev.com>
2019-01-15 10:31:14 +1300

    Update paths to client assets

--- src/Controllers/ElementSpotlightListController.php
+++ src/Controllers/ElementSpotlightListController.php
@@ -10,7 +10,7 @@ class ElementSpotlightListController extends ElementController
   {
     parent::init();
 
-    Requirements::css('dnadesign/silverstripe-elemental-carbon-spotlight: client/css/element-spotlight-list.css');
-    Requirements::css('dnadesign/silverstripe-elemental-carbon-spotlight: client/css/element-spotlight.css');
+    Requirements::css('dnadesign/silverstripe-elemental-spotlights: client/css/element-spotlight-list.css');
+    Requirements::css('dnadesign/silverstripe-elemental-spotlights: client/css/element-spotlight.css');
   }
 }
